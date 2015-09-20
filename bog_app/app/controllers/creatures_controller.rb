4f3d61dbdc6f08ea441584af65a39638a1cfed0a
monaghan.darragh@gmail.com <monaghan.darragh@gmail.com>
2015-09-20 11:44:18 -0700

    stage 18 commit

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -6,13 +6,20 @@ class CreaturesController < ApplicationController
 	end
 
 	def new
+		@creature = Creature.new
 		render: new
 	end
 
 	def create
 		new_creature = params.require(:creature).permit(:name, :description)
 		Creature.create(new_creature)
-		redirect_to '/creatures'
+		redirect_to '/creatures/#{creature.id}'
+	end
+
+	def show 
+		id = params[:id]
+		@creature = Creature.find(id)
+		render :show
 	end
 
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'creatures#index'
 
   get '/creatures', to: 'creatures#index'
-  get '/creatures', to: 'creatures#new'
+  get '/creatures/new', to: 'creatures#new'
+  get '/creatures/:id', to: 'creatures#show' 
   post '/creatures', to: 'creatures#create'
+
 
 end
